95bab68c5ba59462cc5ba21b8a3d57dec78d0fee
Ilyess <rachedi.ilyess@gmail.com>
2024-05-22 12:46:00 +0200

    add descriptive message

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -7,10 +7,27 @@ export class RedisService {
   private client: Redis;
 
   constructor(private configService: ConfigService) {
-    this.client = new Redis({
-      host: this.configService.get<string>('REDIS_HOST'),
-      port: this.configService.get<number>('REDIS_PORT'),
+    console.log('🚀  Initialisation du service Redis... 😊');
+    const host = this.configService.get<string>('REDIS_HOST');
+    const port = this.configService.get<number>('REDIS_PORT');
+
+    if (!host || !port) {
+      console.error(
+        '❌  Erreur: REDIS_HOST ou REDIS_PORT ne sont pas définis 😢',
+      );
+      throw new Error('REDIS_HOST ou REDIS_PORT ne sont pas définis');
+    }
+
+    console.log('⚙️  Création du client Redis avec les paramètres: ', {
+      host,
+      port,
     });
+    this.client = new Redis({
+      host: host,
+      port: port,
+    });
+
+    console.log('✅  Client Redis créé avec succès');
   }
 
   getClient(): Redis {
